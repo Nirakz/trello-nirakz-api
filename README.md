eb88dac93ca6e9cd797e5f7d14604df48ddc7403
nirakz <ntchinh271099@gmail.com>
2022-04-22 21:53:20 +0700

    init project nodejs

---
+++ README.md
@@ -0,0 +1,14 @@
+ # Nodejs + Expressjs + Mongodb - Building an API app like Trello
+
+# Requirments
+
+* nodejs >= v14.7.0
+* npm >= v6.4.7
+* yarn >= v1.22.17
+
+Clone project and run test.
+
+$ git clone https://git_url_clone <project_dir>
+$ cd <project_dir>
+$ yarn install
+$ yarn start
---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { mapOrder } from './utilities/sorts.js'
+
+const  app = express()
+
+const  hostname = 'localhost'
+const  port = 3020
+
+app.get('/', (req, res) => {
+    res.end('<h1>Hello word</h1><hr/>')
+})
+
+app.listen(port, hostname, () => {
+  console.log(`Running at ${hostname}:${port}/`)
+})
