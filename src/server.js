29ce0c22002129e173fbe2d253738e3b0637f9bf
nirakz <ntchinh271099@gmail.com>
2022-04-25 17:00:14 +0700

    Design Database Schema & Manage mongodb

--- src/server.js
+++ src/server.js
@@ -2,14 +2,23 @@ import express from 'express'
 import { connectDB } from '@/config/mongodb'
 import { env } from '@/config/environtment'
 
-const app = express()
+connectDB()
+  .then(() => console.log('Connected successfully to database!'))
+  .then(() => bootServer())
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
 
-connectDB().catch(console.log)
+const bootServer = () => {
+  const app = express()
 
-app.get('/', (req, res) => {
-  res.end('<h1>Hello word</h1><hr/>')
-})
+  app.get('/test', async (req, res) => {
 
-app.listen(env.PORT, env.HOST, () => {
-  console.log(`Running at ${env.HOST}:${env.PORT}/`)
-})
+    res.end('<h1>Hello word</h1><hr/>')
+  })
+
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`Running at ${env.APP_HOST}:${env.APP_PORT}/`)
+  })
+}
